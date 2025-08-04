014096610dabf6d706d2f9da2c88d762d801420b
Ahmad Rasheed <ahmad.r.tec@gmail.com>
2025-08-04 18:53:39 +0500

    Reactions on post updated

--- FitMind-Forum/src/app/website/posts/posts.component.ts
+++ FitMind-Forum/src/app/website/posts/posts.component.ts
@@ -5,6 +5,9 @@ import { GetUserPostsDTO } from '../../Model/GetUserPosts';
 import { MasterService } from '../../Shared/master.service';
 import { SnackBarServiceService } from '../../Shared/snack-bar-service.service';
 import { GetAllPostsDTO } from '../../Model/GetAllPostsDTO';
+import { PostReactionsDTO } from '../../Model/AddPostReaction';
+import { GetPostReactionsCount } from '../../Model/GetPostReactionsCount';
+import { AuthService } from '../../Shared/auth.service';
 
 @Component({
   selector: 'app-posts',
@@ -16,13 +19,45 @@ export class PostsComponent implements OnInit {
   constructor() {}
   posts!: GetAllPostsDTO[];
   MasterService = inject(MasterService);
+  AuthService = inject(AuthService);
   snackBarService = inject(SnackBarServiceService);
+  userId: number = Number(sessionStorage.getItem('appUserId'));
+  postReactionsCount: GetPostReactionsCount | null = null;
+  reactionIcons: boolean = false;
+
   ngOnInit() {
-    this.getAllPosts();
+    this.AuthService.appUserId$.subscribe((userId) => {
+      if (userId) {
+        this.userId = userId;
+      }
+      this.getAllPosts();
+    });
   }
 
+  getPostReactionsCount(postId: number) {
+    this.MasterService.getPostReactionsCount(postId).subscribe(
+      (response) => {
+        console.log('Post reactions count:', response);
+      //  return response;
+      },
+      (error) => {
+        console.error('Error fetching post reactions count:', error);
+        // this.snackBarService.showError('Error fetching post reactions count');
+      }
+    );
+  }
+
+
   getAllPosts() {
-    this.MasterService.getAllPosts().subscribe((posts: GetAllPostsDTO[]) => {
+    // debugger;
+     const userId = this.AuthService.userIdExists();
+     if (userId === 0) {
+      this.userId = 0;
+     }
+     debugger;
+
+    this.MasterService.getAllPosts(this.userId).subscribe((posts: GetAllPostsDTO[]) => {
+
       this.posts = posts;
       console.log('Posts fetched:', this.posts);
     });
@@ -58,5 +93,77 @@ export class PostsComponent implements OnInit {
     if (interval >= 1)
       return `${interval} minute${interval > 1 ? 's' : ''} ago`;
     return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+  }
+
+
+  getPostReactionCount(postId: number) {
+    this.MasterService.getPostReactionsCount(postId).subscribe(
+      (response : GetPostReactionsCount) => {
+        console.log('Post reactions count:', response);
+        // this.postReactionsCount = response;
+        return response;
+      },
+      (error) => {
+        console.error('Error fetching post reactions count:', error);
+        this.snackBarService.showError('Error fetching post reactions count');
+      }
+    );
+  }
+
+  reactionButton: boolean = false;
+  // Function to handle post reaction
+  addPostReaction(isLike: boolean | null,post:GetAllPostsDTO ) {
+    // debugger;
+    this.reactionButton = true;
+    if (!this.AuthService.isLoggedIn()) {
+      this.snackBarService.showError('Please log in to react to posts');
+    this.reactionButton = false;
+
+      return;
+    }
+    if (isLike === null) {
+      this.snackBarService.showError('Please select a reaction');
+    this.reactionButton = false;
+
+      return;
+    }
+     const userId = this.AuthService.userIdExists();
+
+    const postReaction: PostReactionsDTO = {
+      postId: post.postId,
+      userId: userId,
+      isLike: isLike,
+    };
+
+    this.MasterService.addPostReaction(postReaction).subscribe(
+      (response) => {
+        // console.log('Reaction added:', response);
+    this.reactionButton = false;
+
+        this.getAllPosts();
+        // this.snackBarService.showSuccess('Reaction added successfully');
+      },
+      (error) => {
+        // if (error.status === 400) {
+        //  this.MasterService.updatePostReaction(postReaction).subscribe({
+        //     next: (updatedReaction) => {
+        //       console.log('Reaction updated:', updatedReaction);
+        //       this.getAllPosts();
+        //       this.snackBarService.showSuccess('Reaction updated successfully');
+        //     },
+        //     error: (updateError) => {
+        //       console.error('Error updating reaction:', updateError);
+        //       this.snackBarService.showError('Error updating reaction');
+        //     }
+        //  });
+
+        //   return;
+        // }
+        // console.error('Error adding reaction:', error);
+         this.reactionButton = false;
+
+        this.snackBarService.showError('Error adding reaction');
+      }
+    );
   }
 }
